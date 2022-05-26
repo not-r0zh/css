26f52907acd330bdd124176dd519cc3ef36cb597
rozh <roberuto.kun.16@gmail.com>
2022-05-26 19:34:34 +0200

    .

--- hovernumber/script.js
+++ hovernumber/script.js
@@ -1,2 +1,7 @@
-let number = document.getElementById("number")
-number.addEventListener("mouseover",()=> console.log("XD"))
+function myFunction() {
+    var x = document.getElementById("text").value;
+    var r = document.querySelector(":root")
+    var y = document.querySelector("p:hover")
+    r.style.setProperty('--num2',x)
+    y.style.setProperty('font-size',x + 'vmax')
+}
